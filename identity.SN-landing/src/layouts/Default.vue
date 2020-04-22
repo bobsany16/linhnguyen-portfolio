aff41c7d676f186ae1069011c7d465a7911c7b32
Linh Nguyen <linh.ng754@gmail.com>
2020-04-21 21:38:36 -0400

    Styling (1)

--- identity.SN-landing/src/layouts/Default.vue
+++ identity.SN-landing/src/layouts/Default.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="layout">
+  <div class="w3-mobile layout">
     <header class="header">
       <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+        <g-link to="/">identity.SN</g-link>
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
+        <g-link class="nav__link" to="/">Product</g-link>
+        <g-link class="nav__link" to="/About">About</g-link>
+        <g-link class="nav__link" to="/">Contact</g-link>
       </nav>
     </header>
     <slot/>
@@ -21,30 +23,3 @@ query {
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
--- identity.SN-landing/src/main.js
+++ identity.SN-landing/src/main.js
@@ -2,8 +2,14 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import '../w3.css'
+import '../custom.css'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  /*head.link.push({
+    rel: 'stylesheet',
+    href: 'https://www.w3schools.com/w3css/4/w3.css'
+  })*/
 }
