f04475f4ce58d7676930e1776a7ef003f5651820
Linh Nguyen <linh.ng754@gmail.com>
2020-04-29 19:39:25 -0400

    MAKEOVER(1)

--- linhnguyen-ver2/src/components/AboutMe.vue
+++ linhnguyen-ver2/src/components/AboutMe.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="w3-container w3-theme-d3 w3-mobile about-section">
-    <div class="w3-container w3-mobile about-main">
-      <h2>About me</h2>
+  <div id="About" class="about-wrapper">
+    <!--<VerticalHomeBar />-->
+    <div class="w3-container w3-mobile about-section">
+      <div class="w3-container w3-half w3-mobile about-main">
+        <p>
+          I am an aspiring software developer with experience in mobile & web
+          applications. I think design and technology are like peanut butter and
+          jelly. I am on a journey to gain more skills in both of these fields
+          through a dream role of
+          <b class="w3-theme2">frontend developer</b>.<br /><br />
+          I am <b class="w3-theme2">motivated</b>,
+          <b class="w3-theme2">determined</b>, and
+          <b class="w3-theme2">ambitious</b>. Currently going strong on the
+          #100DaysOfCode challenge <br /><br />Success to me is when
+          <em
+            ><b class="w3-theme2"
+              >I go to bed knowing I tried my absolute best for the day</b
+            ></em
+          >.
+        </p>
+      </div>
     </div>
   </div>
 </template>
--- linhnguyen-ver2/src/components/NavBar.vue
+++ linhnguyen-ver2/src/components/NavBar.vue
@@ -1,13 +1,13 @@
 <template>
-  <header class="header w3-theme-d3">
-    <strong>
-      <g-link to="/">Linh Nguyen</g-link>
-    </strong>
+  <header class="header">
+    <div>
+      <g-link class="nav__link2 w3-hover-text-theme3" to="/">LINH NGUYEN</g-link>
+    </div>
     <nav class="nav">
-      <g-link class="nav__link w3-hover-text-theme" to="/">Home</g-link>
-      <g-link class="nav__link w3-hover-text-theme" to="/About">About</g-link>
-      <g-link class="nav__link w3-hover-text-theme" to="/Projects">Projects</g-link>
-      <g-link class="nav__link w3-hover-text-theme" to="/Resume">Contact</g-link>
+      <g-link class="nav__link w3-hover-text-theme2" to="/">HOME</g-link>
+      <g-link class="nav__link w3-hover-text-theme2" to="/About">ABOUT</g-link>
+      <g-link class="nav__link w3-hover-text-theme2" to="/Projects">PROJECTS</g-link>
+      <g-link class="nav__link w3-hover-text-theme2" to="/Resume">RESUME</g-link>
     </nav>
   </header>
 </template>
