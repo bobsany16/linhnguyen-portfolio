2f4d4c6c85eb55c24d6fffe4fb3a3fc6fe8b3907
Linh Nguyen <linh.ng754@gmail.com>
2020-04-28 23:22:30 -0400

    Styled Projects

--- linhnguyen-ver2/src/pages/About.vue
+++ linhnguyen-ver2/src/pages/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about-wrapper">
-    <VerticalHomeBar />
+    <!--<VerticalHomeBar />-->
     <div class="w3-container w3-theme-d3 w3-mobile about-section">
       <div class="w3-container w3-half w3-mobile about-main">
         <h2>About me</h2>
