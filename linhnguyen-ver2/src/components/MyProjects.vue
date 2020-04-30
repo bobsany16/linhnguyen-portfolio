016abe87f6df67b47144291372b992a82328b9eb
Linh Nguyen <linh.ng754@gmail.com>
2020-04-29 23:14:51 -0400

    Added pics

--- linhnguyen-ver2/src/components/MyProjects.vue
+++ linhnguyen-ver2/src/components/MyProjects.vue
@@ -1,23 +1,16 @@
 <template>
-  <!--<div class="project-wrapper">
-    <div class="w3-container w3-mobile project-section">
-      <div class="w3-container w3-half w3-mobile projects-main">
-        
-      </div>
-    </div>
-  </div>-->
   <div class="projects_items">
-    <div class="w3-container projects_item_scrollable">
-      <div class="project_box">
-        <h2 class="w3-round" id="proj_box_title">
-          Austin Thomas's Fitness App
+    <div class="projects_item_scrollable">
+      <div class="project_box w3-theme-l4">
+        <h2 id="proj_box_title">
+          <em>AUSTIN THOMAS'S FITNESS APP</em>
         </h2>
         <img
           class="w3-image w3-round img-proj"
           src="https://cdn.dribbble.com/users/4083085/screenshots/10067486/media/a983b9124e1cad4fc3e7064d1b925d4e.png"
         />
         <div class="w3-container hoverbox-section">
-          <div class="hover_box w3-white w3-round-xlarge w3-hover-theme2">
+          <div class="hover_box w3-white w3-hover-theme2">
             <a
               href="https://github.com/bobsany16/fitness-club-application/tree/master"
               target="_blank"
@@ -25,7 +18,7 @@
               ><p id="code_box">View Code</p></a
             >
           </div>
-          <div class="hover_box w3-white w3-round-xlarge w3-hover-theme2">
+          <div class="hover_box w3-white w3-hover-theme2">
             <a
               href="https://dribbble.com/shots/10067486-Fitness-Application"
               target="_blank"
@@ -35,52 +28,106 @@
           </div>
         </div>
       </div>
-      <div class="project_box">
-        <h2 class="w3-round" id="proj_box_title">
-          Tip Calculator
+      <div class="project_box w3-theme-l5">
+        <h2 id="proj_box_title">
+          <em>IDENTITY.SN</em>
         </h2>
-        <div class="w3-container hoverbox-section">
-          <div class="hover_box w3-round-xlarge w3-hover-theme2">
+        <p class="proj_desc">
+          A social network app made by Team Bonnies for
+          <b>Code:Buffalo Hackathon @Home</b> by <b>43North</b><br />
+          Role: Frontend developer. I took care of styling this web app and
+          worked closely with teammates to develop this app in 14 days.
+        </p>
+        <img
+          class="w3-image w3-round img-proj"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/63508696261277.5eaa30a555232.png"
+        />
+        <div class="w3-container w3-middlebottom hoverbox-section">
+          <div class="hover_box w3-white w3-hover-theme2">
             <a
-              href="https://github.com/bobsany16/Tip-Calculator-Java-Swing"
+              href="https://github.com/mmakutonin/identity.SN"
               target="_blank"
               id="links_text2"
               ><p id="code_box">View Code</p></a
+            >
+          </div>
+          <div class="hover_box w3-white w3-hover-theme2">
+            <a
+              href="https://github.com/mmakutonin/identity.SN"
+              target="_blank"
+              id="links_text2"
+              ><p id="code_box">View Demo</p></a
+            >
+          </div>
+
+          <div class="hover_box w3-white w3-hover-theme2">
+            <a
+              href="https://www.youtube.com/watch?v=V2tLDdPnyl4"
+              target="_blank"
+              id="links_text2"
+              ><p id="code_box">View Pitch Video</p></a
             >
           </div>
         </div>
       </div>
     </div>
-    <!--<div class="w3-container projects_item_scrollable">
-      <div class="projects_item_self">
-        <div class="project_box">
-          <h2 class="w3-round" id="proj_box_title">
-            Fubuki Puzzle Solver
-          </h2>
-          <div class="hover_box w3-round-xlarge w3-hover-theme2">
+
+    <div class="projects_item_scrollable">
+      <div class="project_box w3-theme-l2">
+        <h2 id="proj_box_title">
+          <em>ANOMALY DETECTION RESEARCH</em>
+        </h2>
+        <p class="proj_desc">
+          Machine Learning project for class. Jason Green and I assessed anomaly
+          detection in 2016 US Presidential Election datasets.
+        </p>
+        <img class="w3-image w3-round img-proj" src="/stats.png" />
+        <div class="w3-container hoverbox-section">
+          <div class="hover_box w3-white w3-hover-theme2">
             <a
-              href="https://github.com/bobsany16/fubukiPuzzleSolver"
+              href="https://github.com/bobsany16/Anomaly-Detection-Research"
               target="_blank"
               id="links_text2"
               ><p id="code_box">View Code</p></a
             >
           </div>
+          <div class="hover_box w3-white w3-hover-theme2">
+            <a id="links_text2"><p id="code_box">App: Coming Soon</p></a>
+          </div>
         </div>
-        <div class="project_box">
-          <h2 class="w3-round" id="proj_box_title">
-            Automated Tweet Delete Bot
-          </h2>
-          <div class="hover_box w3-round-xlarge w3-hover-theme2">
+      </div>
+      <div class="project_box w3-theme-l1">
+        <h2 id="proj_box_title">
+          <em>#100DaysOfCode</em>
+        </h2>
+        <p class="proj_desc ">
+          I made a commitment to complete 100DaysOfCode challenge. I am learning so much. I also earned
+          freeCodeCamp's Responsive Web Design certification! 
+        </p>
+        <img
+          class="w3-image w3-round img-proj"
+          src="/proud.png"
+        />
+        <div class="w3-container w3-middlebottom hoverbox-section">
+          <div class="hover_box w3-white w3-hover-theme2">
             <a
-              href="https://github.com/bobsany16/Automated_Tweet_Delete"
+              href="https://github.com/bobsany16/100-days-of-code/blob/master/log.md"
               target="_blank"
               id="links_text2"
-              ><p id="code_box">View Code</p></a
+              ><p id="code_box">View Log</p></a
+            >
+          </div>
+          <div class="hover_box w3-white w3-hover-theme2">
+            <a
+              href="https://www.freecodecamp.org/certification/bobsany16/responsive-web-design"
+              target="_blank"
+              id="links_text2"
+              ><p id="code_box">View Cert</p></a
             >
           </div>
         </div>
       </div>
-    </div>-->
+    </div>
   </div>
 </template>
 
