459a4c96f678fabdaf182dfa2c65297d11e857dc
Linh Nguyen <linh.ng754@gmail.com>
2020-05-07 18:54:19 -0400

    Changed site name

--- linhnguyen-ver2/src/pages/Resume.vue
+++ linhnguyen-ver2/src/pages/Resume.vue
@@ -1,29 +1,18 @@
 <template>
-
-    <!--<VerticalHomeBar />-->
-    <!--<div class="w3-container w3-theme-d3 w3-mobile about-section">
-      <div class="w3-container w3-mobile about-main">
-        <h2>My Orchids</h2>
-      </div>
-    </div>-->
-    <pdf src="./NGUYEN_RESUME.pdf" style="height: 100%"></pdf>
-    
- 
-    
+  <pdf src="./Nguyen_Resume.pdf"></pdf>
 </template>
 
 <script>
 import VerticalHomeBar from "../components/VerticalHomeBar";
-import pdf from 'vue-pdf';
+import pdf from "vue-pdf";
 
 export default {
   metaInfo: {
     title: "My Resumé",
   },
   components: {
-      VerticalHomeBar,
-      pdf,
-      
+    VerticalHomeBar,
+    pdf,
   },
 };
 </script>
