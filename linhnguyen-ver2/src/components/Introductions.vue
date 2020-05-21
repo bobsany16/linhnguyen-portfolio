b716e27342f26648cf0ce0cd5edf9e77ec42bc9c
Linh Nguyen <linh.ng754@gmail.com>
2020-05-21 18:37:37 -0400

    Added orchids section

---
+++ linhnguyen-ver2/src/components/Introductions.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w3-container w3-mobile home-main">
+    <h3><em>HELLO.</em></h3>
+    <h1><em>I'M LINH</em></h1>
+
+    <div class="subtitle w3-mobile">
+      <p>Software Developer, <b>LGBTQ+</b> Advocate, and Orchid Enthusiast</p>
+    </div>
+    <ContactsIcon />
+  </div>
+</template>
+
+<script>
+import ContactsIcon from "./ContactsIcon";
+export default {
+  name: "Introductions",
+  components: {
+    ContactsIcon,
+  },
+};
+</script>
